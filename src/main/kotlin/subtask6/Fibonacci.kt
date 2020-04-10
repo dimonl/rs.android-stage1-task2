bbfd1ac7ae99ea7ac4d4af99272ef8ced5bfc3a2
Dzmitri <dzmitrilituyev@gmail.com>
2020-04-10 18:51:26 +0300

    last one

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,19 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
+    var oneEl:Int=0
+    var secondEl:Int=1
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        while (true){
+            when {
+                (oneEl*secondEl == n) -> return intArrayOf(oneEl,secondEl,1)
+                (oneEl*secondEl > n) -> return intArrayOf(oneEl,secondEl,0)
+                else -> {
+                    var temp = oneEl
+                    oneEl = secondEl
+                    secondEl = secondEl+temp
+                }
+            }
+        }
     }
 }
